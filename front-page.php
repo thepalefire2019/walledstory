578f1011c053b4cb15df7c10596598f5ef1344d5
sarasij <sarasijoffice@gmail.com>
2019-12-22 13:49:57 +0530

    front design

--- front-page.php
+++ front-page.php
@@ -125,6 +125,53 @@ get_header();
 			</div>
 
 		</article> <!--Gold -->
+		<div class="space30"></div>
+		<article class="most-viewed-parent" style="background:url('<?php echo get_stylesheet_directory_uri() ?>/img/banner.jpg');">
+			<div class="shape">
+				<img src="<?php echo get_stylesheet_directory_uri() ?>/img/shape.png">
+			</div>
+			<div class="head">
+				<h1>Top Viewed Post</h1>
+				<p>Have a look at the top viewed posts.</p>
+			</div>
+			<div class="post-box">
+				<div class="container">
+					<div class="row">
+						<?php 
+						$most_viewed_counter = 0;
+						$temp = 0;
+						while( $temp <= 2 ){
+							if( $most_viewed_counter == 0 ){
+						?>
+						<div class="col-md-4">
+							<div class="single-box-black">
+								<h1>This is the heading of the post which can be a large one.</h1>
+								<p>This is some dummy text for the tseting of the site and the designning. The more the content the more the design will be understood so i am making up this shit just to increase.</p>
+								<a href="#"><h2>Read</h2></a>
+							</div>
+						</div>
+						<?php 
+							$most_viewed_counter++;
+							$temp++;
+							continue;
+							}//end of if?>
+						<div class="col-md-4">
+							<div class="single-box-white">
+								<h1>This is the heading of the post which can be a large one.</h1>
+								<p>This is some dummy text for the tseting of the site and the designning. The more the content the more the design will be understood so i am making up this shit just to increase.</p>
+								<a href="#"><h2>Read</h2></a>
+							</div>
+						</div>
+						<?php 
+						$most_viewed_counter++;
+						$temp++;
+						}// end of while
+						?>
+						
+					</div>
+				</div>
+			</div>
+		</article> <!--Most Viewed -->
 		
 		<div class="space30"></div>
 
@@ -154,6 +201,50 @@ get_header();
 
 		</article> <!--silver -->
 		<div class="space30"></div>
+
+
+		<article class="all-categories">
+			<div class="container">
+				<h1>More Posts on</h1>
+				<p class="underline-p"></p>
+				<div class="row">
+					<?php
+					$categories = get_terms([
+					    'taxonomy' => 'english_category',
+					    'hide_empty' => false,
+					]);
+					$category_counter = 1;
+					foreach( $categories as $category ){
+						if( $category_counter == 13 ){
+							break;
+						}
+					$cat_color_id = "eng_category_" . $category->term_id;
+					$eng_cat_color = get_option($cat_color_id);
+					if($eng_cat_color){
+						$eng_cat_bgcolor = $eng_cat_color['color'];
+					}else{
+						$eng_cat_bgcolor = '#00D99F';
+					}
+					?>
+					<div class="col-md-2 col-6">
+						<a href="#">
+							<div class="cat-box" style="background:<?php echo$eng_cat_bgcolor; ?>">
+								<p><?php echo $category->name; ?></p>
+							</div>
+						</a>
+					</div>
+					<?php 
+					$category_counter++;
+					} 
+
+					?>
+
+				</div>	
+			</div>
+				
+		</article>
+		<div class="space30"></div>
+
 
 		<article class="front-page-bronze">
 			<div class="front-page-bronze-background"></div>
